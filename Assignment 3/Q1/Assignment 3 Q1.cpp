85278dc66915bdec59d74ce21601308373178540
gavinbarton7 <gavinbarton@gmail.com>
2024-11-06 17:48:37 -0600

    Add files via upload

---
+++ Assignment 3/Q1/Assignment 3 Q1.cpp
@@ -0,0 +1,45 @@
+#include "Assignment 3 Q1.h"
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	// creates a queue named prgramQueue that stores integer values
+	Queue<int> programQueue;
+
+	
+	// inserts the first 10 integers (1-10) into the queue
+	for (int i = 1; i <= 10; i++)
+	{
+		programQueue.push(i);
+	} 
+
+
+	// Assigns the newly created variable "queueSize" the value of the current size of programQueue
+	int queueSize = programQueue.size();
+
+	// Display each of the values in programQueue in order from front to rear by output the front value, moving it to the rear, and
+	// repeating the process as many times as the size of programQueue
+	cout << "Oringinal Queue: ";
+	for (int i = 0; i < queueSize; i++) 
+	{ 
+		cout << programQueue.front() << " ";
+		programQueue.move_to_rear();
+	} 
+	cout << endl << endl;
+
+	// calls the "move_to_rear" function to move the value that is currently at the front of programQueue to the rear of programQueue
+	programQueue.move_to_rear();
+
+	// Display each of the values in programQueue in order from front to rear by output the front value, moving it to the rear, and
+	// repeating the process as many times as the size of programQueue
+	cout << "Queue with front element moved to rear: ";
+	for (int i = 0; i < queueSize; i++) 
+	{ 
+		cout << programQueue.front() << " ";
+		programQueue.move_to_rear();
+	} 
+	cout << endl;
+	
+	return 0;
+}
